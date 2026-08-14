6945c768f05c1bcde45fed6ff26180515f750e3d
Trixia Albelda <trixiaalbelda@Trixias-MacBook-Air.local>
2026-08-14 13:06:18 +0800

    trigger - 13 test volume mount secrets

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import express from 'express';
+import fs from 'fs';
+import path from 'path';
+
 const app = express();
+const SECRETS_DIR = '/mnt/secrets';
 //8
 app.listen(8080, () => {
  console.log('Server running on port 8080');
@@ -7,7 +11,17 @@ app.listen(8080, () => {
 
 app.get('/', (req, res, next) => {
   console.log("TEST LOG");
+
+  const mountedSecrets = {};
+  if (fs.existsSync(SECRETS_DIR)) {
+    for (const name of fs.readdirSync(SECRETS_DIR)) {
+      mountedSecrets[name] = fs.readFileSync(path.join(SECRETS_DIR, name), 'utf8');
+    }
+  }
+
   res.json({
-    message: `Hello, World! -- ${process.env.TEST_VAULT_1}`
+    message: 'Hello, World!',
+    env: process.env,
+    mountedSecrets
   });
 });
